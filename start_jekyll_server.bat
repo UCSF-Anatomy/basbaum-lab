863539c1334012f844bd30ce59817b3838f227ef
Biafra Ahanonu <bahanonu@alum.mit.edu>
2020-10-06 12:32:06 -0700

    Update former lab members and misc. updates
    - Added all former postdocs and graduate students.
    - Added `start_jekyll_server.bat` and `start_jekyll_server.sh` for testing on local machine.

---
+++ start_jekyll_server.bat
@@ -0,0 +1,6 @@
+rem Biafra Ahanonu
+rem Opens default localhost and starts jekyll server with baseurl in dev mode
+rem 2020.10.05 [17:35:18]
+
+start "" "http://localhost:4000/"
+jekyll serve --baseurl ""
---
+++ start_jekyll_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Biafra Ahanonu
+# Opens default localhost and starts jekyll server with baseurl in dev mode
+# 2020.10.05 [17:35:18]
+
+# node server.js &
+xdg-open http://localhost:4000/
+jekyll serve --baseurl ""
